3a23c06c6ffd942e15711947f8de4f086f212c64
eivanov2 <eivanov2@myune.edu.au>
2026-09-09 02:24:57 +1000

    add svc skeleton

---
+++ svc.py
@@ -0,0 +1,20 @@
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from preprocessing import get_features_and_labels, get_cv_splitter
+
+
+def create_svc_model():
+    """Create the SVC model..."""
+    pass
+
+
+def train_svc(X, y):
+    """Train the SVC model.."""
+    pass
+
+
+def evaluate_svc(model, X, y):
+    """Evaluate the trained SVC model..."""
+    pass

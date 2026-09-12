640ee7a28b4bd50de58fb72a793c989fe134fe42
chansen-une <chansen9@myune.edu.au>
2026-09-13 00:34:28 +0300

    added confusion matrix func

--- visualizations.py
+++ visualizations.py
@@ -7,5 +7,14 @@ uses data output from models defined in knn.py, svc.py and rforest.py
 """
 
 
-from sklearn import ConfusionMatrixDisplay
+from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(y_true, y_pred, labels, title):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ConfusionMatrixDisplay.from_predictions(
+        y_true, y_pred, labels=labels, cmap=plt.cm.Blues, ax=ax,
+        xticks_rotation=45
+    )
+    ax.set_title(title)
+    plt.show()

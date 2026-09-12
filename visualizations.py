b91aa7a73b0d1740bf4692f752f5cbb36be10bca
chansen-une <chansen9@myune.edu.au>
2026-09-13 00:25:06 +0300

    added  visuallizations.py file for chart templates

---
+++ visualizations.py
@@ -0,0 +1,11 @@
+"""
+File for visualization templates 
+
+used by: data_analysis.ipynb
+uses data output from models defined in knn.py, svc.py and rforest.py
+
+"""
+
+
+from sklearn import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
